f3f55eb3431df7ebee290049684c6c8acecde2e7
Boris Slobodin <boris.slobodin@jadedpixel.com>
2014-03-25 21:20:44 +0000

    Add signature validation; Clean up

--- app.rb
+++ app.rb
@@ -4,41 +4,55 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 require 'addressable/uri'
+require 'byebug' if development?
 
-post '/' do
-  erb :index
-end
+class OffsiteGatewaySim < Sinatra::Base
 
-post '/execute/:action' do |action|
-  key = 'iU44RWxeik'
-  fields = request.params.select {|k, v| k.start_with? 'x-'}
-  ts = Time.now.utc.iso8601
-  payload = {
-    'x-id'                => fields['x-id'],
-    'x-reference'         => fields['x-reference'],
-    'x-currency'          => fields['x-currency'],
-    'x-test'              => fields['x-test'],
-    'x-amount'            => fields['x-amount'],
-    'x-result'            => action,
-    'x-gateway-reference' => SecureRandom.hex,
-    'x-timestamp'         => ts
-    }
-  payload['x-signature'] = Digest::HMAC.hexdigest(payload.sort.join, key, Digest::SHA1)
-
-  result = {timestamp: ts}
-  redirect_url = Addressable::URI.parse(fields['x-url-complete'])
-  redirect_url.query_values = payload
-  debugger
-  if request.params['fire_callback'] == 'true'
-    callback_url = fields['x-url-callback']
-    response = HTTParty.post(callback_url, body: payload)
-    if response.code == 200
-      result[:redirect] = redirect_url
-    else
-      result[:error] = response
-    end
-  else
-    result[:redirect] = redirect_url
+  def initialize
+    @key = 'iU44RWxeik'
+    super
   end
-  result.to_json
+
+  def sign(fields)
+    Digest::HMAC.hexdigest(fields.sort.join, @key, Digest::SHA256)
+  end
+
+  post '/' do
+    @fields = request.params.select {|k, v| k.start_with? 'x-'}
+    signature_ok = sign(@fields.reject{|k,_| k == 'x-signature'}) == @fields['x-signature']
+    erb :index, :locals => {signature_ok: signature_ok}
+  end
+
+  post '/execute/:action' do |action|
+    ts = Time.now.utc.iso8601
+    payload = {
+      'x-id'                => @fields['x-id'],
+      'x-reference'         => @fields['x-reference'],
+      'x-currency'          => @fields['x-currency'],
+      'x-test'              => @fields['x-test'],
+      'x-amount'            => @fields['x-amount'],
+      'x-result'            => action,
+      'x-gateway-reference' => SecureRandom.hex,
+      'x-timestamp'         => ts
+      }
+    payload['x-signature'] = sign(payload)
+    result = {timestamp: ts}
+    redirect_url = Addressable::URI.parse(@fields['x-url-complete'])
+    redirect_url.query_values = payload
+    if request.params['fire_callback'] == 'true'
+      callback_url = @fields['x-url-callback']
+      response = HTTParty.post(callback_url, body: payload)
+      if response.code == 200
+        result[:redirect] = redirect_url
+      else
+        result[:error] = response
+      end
+    else
+      result[:redirect] = redirect_url
+    end
+    result.to_json
+  end
+
+  run! if app_file == $0
+
 end
